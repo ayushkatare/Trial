e331e813048c8eb232edb0746a3c05e6d9a91a9a
ayushkatare <ayushkatare123@gmail.com>
2019-01-26 17:23:56 +0530

    first commite

---
+++ src/main/java/com/example/model/Person.java
@@ -0,0 +1,52 @@
+package com.example.model;
+
+public class Person {
+
+    private String name;
+    private int id;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String Name, int Age, int Id){
+        this.name = Name;
+        this.age = Age;
+        this.id = Id;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                '}';
+    }
+
+
+
+
+}
